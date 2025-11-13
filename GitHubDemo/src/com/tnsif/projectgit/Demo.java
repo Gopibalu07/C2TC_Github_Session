d6b5aeaf1d535af2cad51eea61f4848d3460be28
Vignesh-S-vicky <vigneshjack69@gmail.com>
2025-11-13 15:01:42 +0530

    updated by vicky

--- GitHubDemo/src/com/tnsif/projectgit/Demo.java
+++ GitHubDemo/src/com/tnsif/projectgit/Demo.java
@@ -1,7 +1,9 @@
 package com.tnsif.projectgit;
 
 public class Demo {
-	public static void main(String args[]) {
-		System.out.println("Hello world");
+	public static void main(String[] args) {
+		System.out.println("vanakam da mapala villupurathala irundhu");
+		System.out.println("😎🙏");
+		
 	}
 }
